1610d83223d115ae8a7a1cc4d99a4d77d27c47b9
Angeline Marie Michael Meitzler <angelinemeitzler@Angelines-MBP-2.lan>
2021-06-20 23:28:31 -0400

    path fix

--- scripts/archive.js
+++ scripts/archive.js
@@ -3,6 +3,8 @@ const archive = document.querySelector("#screenshots .shot_container");
 const row1 = document.querySelector("#screenshots .shot_container .row1");
 const folder = "/assets/grabs/";
 
+console.log(folder)
+
 $.ajax({
   url : folder,
   success: function (data, i) {
