592b3231a272bd09226272c7c991174a83d043cb
Angeline Marie Michael Meitzler <angelinemeitzler@Angelines-MacBook-Pro-2.local>
2021-06-24 14:20:13 -0500

    gallery fix

--- scripts/iframe.js
+++ scripts/iframe.js
@@ -1,40 +1,40 @@
-const unmute_elm = document.getElementById("unmute-video");
-let tag = document.createElement('script');
-let firstScriptTag = document.getElementsByTagName('script')[0];
-let player;
+// const unmute_elm = document.getElementById("unmute-video");
+// let tag = document.createElement('script');
+// let firstScriptTag = document.getElementsByTagName('script')[0];
+// let player;
 
-tag.src = "https://www.youtube.com/iframe_api";
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// tag.src = "https://www.youtube.com/iframe_api";
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('youtube-video', {
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    },
-  });
-}
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('youtube-video', {
+//     events: {
+//       'onReady': onPlayerReady,
+//       'onStateChange': onPlayerStateChange
+//     },
+//   });
+// }
 
-function onPlayerReady(event) {
-  console.log("hey Im ready");
-  event.target.playVideo();
-  event.target.setVolume(1);
-  event.target.stopVideo();
-  //do whatever you want here. Like, player.playVideo();
-}
+// function onPlayerReady(event) {
+//   console.log("hey Im ready");
+//   event.target.playVideo();
+//   event.target.setVolume(1);
+//   event.target.stopVideo();
+//   //do whatever you want here. Like, player.playVideo();
+// }
 
-function onPlayerStateChange() {
-  console.log("my state changed");
-}
+// function onPlayerStateChange() {
+//   console.log("my state changed");
+// }
 
-unmute_elm.addEventListener('click', function(event) {
-  player.mute();
-  if (player.isMuted()) {
-    player.unMute();
-  }
-});
+// unmute_elm.addEventListener('click', function(event) {
+//   player.mute();
+//   if (player.isMuted()) {
+//     player.unMute();
+//   }
+// });
 
-setTimeout(function(){
-  unmute_elm.click();
-}, 5000);
+// setTimeout(function(){
+//   unmute_elm.click();
+// }, 5000);
 
