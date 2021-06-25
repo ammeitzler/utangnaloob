9ca597f0fd45767085b28c29bbf94da799cb29cf
Angeline Marie Michael Meitzler <angelinemeitzler@Angelines-MacBook-Pro-2.local>
2021-06-25 13:53:50 -0500

    gallery fix

--- scripts/reload.js
+++ scripts/reload.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     // SET FOR INSTALL // RELOADS EVERY 24 HOURS
-    setInterval(function(){ reload_page(); }, 1000 * 60 * 60 * 24);
+   //  setInterval(function(){ reload_page(); }, 1000 * 60 * 60 * 24);
+   setInterval(function(){ reload_page(); }, 10000);
  });
 
  function reload_page() {
     window.location.reload(true);
+    console.log("reload")
  }
  
